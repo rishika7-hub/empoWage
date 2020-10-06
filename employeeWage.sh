8b5e1408b4204b2b253504b5cb5da6e0cd4ac420
rishika7-hub <srishika970@gmail.com>
2020-10-07 00:09:49 +0530

    Employee Attendance

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 echo "Welcome to Employee Wage Computation Problem"
 
+isPresent=1
+randomCheck=$(( RANDOM%2 ));
+if [ $isPresent -eq $randomCheck ];
+then
+		echo "Employee is present"
+else
+		echo "Employee is not present"
+fi
