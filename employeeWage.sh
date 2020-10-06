f55baea1870289c7c377cb250e19ddd74a803b05
rishika7-hub <srishika970@gmail.com>
2020-10-07 00:41:17 +0530

    Functions

--- employeeWage.sh
+++ employeeWage.sh
@@ -126,3 +126,42 @@ do
 done
 
 echo "total Salary = $totalSalary"
+
+isFullTimeEmp=1;
+isPartTimeEmp=2;
+
+wagePerHour=20;
+salary=0;
+totalSalary=0;
+
+empName=emp
+
+echo "calculate wages for a Month"
+
+getWorkHours () {
+days=$1
+empHrs=$2
+while [ $days -le 20 ] || [ $empHrs -le 100 ]
+do
+	employeeCheck=$(( 1 + RANDOM % 2 ));
+	case $employeeCheck in
+		$isFullTimeEmp)
+			empName=FullTimeEmployee
+			empHrs=$(( $empHrs + 8 ));;
+		$isPartTimeEmp)
+			empName=PartTimeEmployee
+			empHrs=$(( $empHrs + 4 ));;
+	esac
+	salary=$(( $empHrs * $wagePerHour ))
+#	echo "salary of $empName on the day $days is $salary"
+#	echo -e
+	totalSalary=$(( $totalSalary + $salary ))
+	((days++))
+done
+}
+
+days=1
+empHrs=0
+getWorkHours $days $empHrs
+echo "total Salary = $totalSalary"
+echo "working hours = " $empHrs
