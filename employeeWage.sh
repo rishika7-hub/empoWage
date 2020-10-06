b57edff206219227c9aaa0f44a663a65c3996389
rishika7-hub <srishika970@gmail.com>
2020-10-07 00:32:55 +0530

    Added Conditions

--- employeeWage.sh
+++ employeeWage.sh
@@ -90,3 +90,39 @@ do
 	echo -e
 	totalSalary=$(( $totalSalary + $salary ))
 done
+
+isFullTimeEmp=1;
+isPartTimeEmp=2;
+
+wagePerHour=20;
+salary=0;
+totalSalary=0;
+
+empName=emp
+
+echo "calculate wages for a Month"
+
+#read -p "enter the number of working days = " numDays
+
+days=1
+empHrs=0
+#for (( days=1; days<=$numDays; days++ ));
+while [ $days -le 20 ] || [ $empHrs -le 100 ]
+do
+	employeeCheck=$(( 1 + RANDOM % 2 ));
+	case $employeeCheck in
+		$isFullTimeEmp)
+			empName=FullTimeEmployee
+			empHrs=$(( $empHrs + 8 ));;
+		$isPartTimeEmp)
+			empName=PartTimeEmployee
+			empHrs=$(( $empHrs + 4 ));;
+	esac
+	salary=$(( $empHrs * $wagePerHour ))
+	echo "salary of $empName on the day $days is $salary"
+	echo -e
+	totalSalary=$(( $totalSalary + $salary ))
+	((days++))
+done
+
+echo "total Salary = $totalSalary"
