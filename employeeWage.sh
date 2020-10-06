d891d4e8d3d70aa29568a4b4685209ff8545c8dd
rishika7-hub <srishika970@gmail.com>
2020-10-07 00:46:56 +0530

    Storing the day and daily wage along with total wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -200,3 +200,38 @@ empHrs=0
 getWorkHours $days $empHrs
 echo "total Salary = $totalSalary"
 echo "working hours = " $empHrs
+
+isFullTimeEmp=1;
+isPartTimeEmp=2;
+wagePerHour=20;
+salary=0;
+totalSalary=0;
+empName=emp
+echo "calculate wages for a Month"
+echo "Day		DailyWages		TotalWages"
+getWorkHours () {
+days=$1
+empHrs=$2
+while [ $days -le 20 ] || [ $empHrs -le 100 ]
+do
+	employeeCheck=$(( 1 + RANDOM % 2 ));
+	case $employeeCheck in
+		$isFullTimeEmp)
+			empName=FullTimeEmployee
+			empHrs=$(( $empHrs + 8 ));;
+		$isPartTimeEmp)
+			empName=PartTimeEmployee
+			empHrs=$(( $empHrs + 4 ));;
+	esac
+	salary=$(( $empHrs * $wagePerHour ))
+	totalSalary=$(( $totalSalary + $salary ))
+	echo "Day $days		$salary			$totalSalary"
+	((days++))
+done
+}
+
+days=1
+empHrs=0
+getWorkHours $days $empHrs
+echo "total Salary = $totalSalary"
+echo "working hours = " $empHrs
