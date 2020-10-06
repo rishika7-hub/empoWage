51a7b950ecf197c2212964d787715a1773db3873
rishika7-hub <srishika970@gmail.com>
2020-10-07 00:16:44 +0530

    Part time employee & wage is added

--- employeeWage.sh
+++ employeeWage.sh
@@ -18,3 +18,13 @@ hoursPerDay=8
 wagePerDay=$(( 20 * 8))
 
 echo "daily employee wages = $wagePerDay"
+
+#echo "Fulltime employee daily wages = $wagePerDay"
+
+echo "calculate Part time Employee & wage"
+
+partTimeHoursPerDay=4
+
+PartTimeWagePerDay=$(( $wagePerHour * $partTimeHoursPerDay ))
+
+echo "Part Time employee daily wages = $PartTimeWagePerDay"
