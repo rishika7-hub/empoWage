2c8dcbf61c04294d83c1e3fae39ed66df25efdde
rishika7-hub <srishika970@gmail.com>
2020-10-07 00:30:13 +0530

    Monthly wages

--- employeeWage.sh
+++ employeeWage.sh
@@ -59,3 +59,34 @@ partTimeHoursPerDay=4
 PartTimeWagePerDay=$(( $wagePerHour * $partTimeHoursPerDay ))
 
 echo "Part Time employee daily wages = $PartTimeWagePerDay"
+
+isFullTimeEmp=1;
+isPartTimeEmp=2;
+
+wagePerHour=20;
+salary=0;
+totalSalary=0;
+
+empName=emp
+
+echo "calculate wages for a Month"
+
+#read -p "enter the number of working days = " numDays
+
+numDays=20
+for (( days=1; days<=$numDays; days++ ));
+do
+	employeeCheck=$(( 1 + RANDOM % 2 ));
+	case $employeeCheck in
+		$isFullTimeEmp)
+			empName=FullTimeEmployee
+			empHrs=8;;
+		$isPartTimeEmp)
+			empName=PartTimeEmployee
+			empHrs=4;;
+	esac
+	salary=$(( $empHrs * $wagePerHour ))
+	echo "salary of $empName on the day $days is $salary"
+	echo -e
+	totalSalary=$(( $totalSalary + $salary ))
+done
